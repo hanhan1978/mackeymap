4f7abf968e382ef6dfc3c1585ab569e47f3efb5c
hanhan <hanhan@hanhans-mac.lan>
2013-01-04 09:56:37 +0900

    とりあえず追加

---
+++ src/net/hanhans/app/mackeymap/KeymapReceiver.java
@@ -0,0 +1,13 @@
+package net.hanhans.app.mackeymap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeymapReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+	{
+    }
+}
